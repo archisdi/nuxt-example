64ec8b2c570674b12dc47e4cd684302b7853049f
Archie <archie.isdiningrat@bizzy.co.id>
2018-05-22 11:30:01 +0700

    checkpoint

---
+++ 01-blog/components/Posts/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+      <div class="post-list">
+      <post-preview id="1" thumbnail="https://images.tech.co/wp-content/uploads/2015/08/techco-web.png" title="Hola Amigos" previewText="this is preview text"></post-preview>
+      <post-preview id="2" thumbnail="https://images.tech.co/wp-content/uploads/2015/08/techco-web.png" title="Hola Amigos" previewText="this is preview text"></post-preview>
+      <post-preview id="3" thumbnail="https://images.tech.co/wp-content/uploads/2015/08/techco-web.png" title="Hola Amigos" previewText="this is preview text"></post-preview>
+  </div>
+</template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
+
+<style>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ 01-blog/layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <the-header @sidenavToggle="displaySidenav = !displaySidenav"></the-header>
+    <the-sidenav :show="displaySidenav" @close="displaySidenav = false"></the-sidenav>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSidenav from "@/components/Navigation/TheSidenav";
+export default {
+  data() {
+    return {
+      displaySidenav: false
+    };
+  },
+  components: {
+    TheHeader,
+    TheSidenav
+  }
+};
+</script>
+
+
+<style>
+html {
+  font-family: "Open Sans", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
